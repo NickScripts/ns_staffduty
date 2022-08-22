8687df535b83a9b8b37ee4d2d29861a7bb244c5f
Nick's Scripts <95194821+NickScripts@users.noreply.github.com>
2022-08-22 16:39:30 -0500

    NON ESX UPDATE

--- ns_staffduty/ns_staffduty_server.lua
+++ ns_staffduty/ns_staffduty_server.lua
@@ -9,37 +9,35 @@ RegisterCommand("staffduty", function(source)
 		if activeDuties[source] then
 			TriggerClientEvent("EasyAdmin:staffduty:toggle", source, false)
 			activeDuties[source] = os.time()
-			local _source = source	
-			local xPlayer = ESX.GetPlayerFromId(_source)    
-				local connect = {
+			local connect = {
 					{
-						["color"] = "16711680",
-						["title"] = 'Player: '  ..GetPlayerName(source).."",
-						["description"] = 'Went off-duty as staff, was on-duty for '..disp_time(os.difftime(os.time(), activeDuties[source])).. "",
-						["footer"] = {
-						["text"] = os.date('%H:%M - %d. %m. %Y', os.time()),
-						["icon_url"] = "",
-						},
-					}
-				}
-				PerformHttpRequest('<DISCORD WEBHOOK GOES HERE>', function(err, text, headers) end, 'POST', json.encode({username = "Nicks Scripts - Easy Admin", embeds = connect, avatar_url = 'https://cdn.discordapp.com/attachments/969028303153426443/1004100536657788988/looted_-_Copy.png'}), { ['Content-Type'] = 'application/json' })
+					["color"] = "16711680",
+					["title"] = "Player "  ..GetPlayerName(source).."",
+					["description"] = "Went on duty as staff.",
+					["footer"] = {
+					["text"] = os.date('%H:%M - %d. %m. %Y', os.time()),
+					["icon_url"] = "",
+					},
+			    }
+		    }	
+			PerformHttpRequest('<DISCORD WEBHOOK GOES HERE>', function(err, text, headers) end, 'POST', json.encode({username = "Nicks Scripts - Staff Duty", embeds = connect, avatar_url = 'https://cdn.discordapp.com/attachments/1011388658617688147/1011388730218659840/ns.gif'}), { ['Content-Type'] = 'application/json' })
 			activeDuties[source] = nil
 		else
 			TriggerClientEvent("EasyAdmin:staffduty:toggle", source, true)
 			local _source = source	
 			local xPlayer = ESX.GetPlayerFromId(_source)    
-				local connect = {
+			local connect = {
 					{
-						["color"] = "16711680",
-						["title"] = 'Player: '  ..GetPlayerName(source).."",
-						["description"] = 'Went on-duty as staff',
-						["footer"] = {
-						["text"] = os.date('%H:%M - %d. %m. %Y', os.time()),
-						["icon_url"] = "",
-						},
-					}
-				}	
-				PerformHttpRequest('<DISCORD WEBHOOK GOES HERE>', function(err, text, headers) end, 'POST', json.encode({username = "Nicks Scripts - Easy Admin", embeds = connect, avatar_url = 'https://cdn.discordapp.com/attachments/969028303153426443/1004100536657788988/looted_-_Copy.png'}), { ['Content-Type'] = 'application/json' })
+					["color"] = "16711680",
+					["title"] = "Player "  ..GetPlayerName(source).."",
+					["description"] = "Went off duty as staff.",
+					["footer"] = {
+					["text"] = os.date('%H:%M - %d. %m. %Y', os.time()),
+					["icon_url"] = "",
+					},
+			    }
+		    }	
+			PerformHttpRequest('<DISCORD WEBHOOK GOES HERE>', function(err, text, headers) end, 'POST', json.encode({username = "Nicks Scripts - Staff Duty", embeds = connect, avatar_url = 'https://cdn.discordapp.com/attachments/1011388658617688147/1011388730218659840/ns.gif'}), { ['Content-Type'] = 'application/json' })
 			activeDuties[source] = os.time()
 		end
 	end
